5c21d31296cb34c67eed6df90864394a889894c0
lai2-01 <lai2-01@itismeucci.com>
2022-10-10 09:01:14 +0200

    First commit

---
+++ progetto_chat_marinelli_enrico_client/src/main/java/it/fi/em/meucci/ClientStr.java
@@ -0,0 +1,63 @@
+package it.fi.em.meucci;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.*;
+import java.net.UnknownHostException;
+
+public class ClientStr {
+    String nomeServer = "localhost";
+    int portaServer = 6789;
+    Socket miosocket;
+    BufferedReader tastiera;
+    String stringaUtente;
+    String stringaRicevutaDalServer;
+    DataOutputStream outVersoServer;
+    BufferedReader inDalServer;
+
+
+    public Socket connetti(){
+        System.out.println("2 CLIENT partit in esecuzione... ");
+
+        try{
+            tastiera = new BufferedReader(new InputStreamReader(System.in));
+
+            miosocket = new Socket(nomeServer, portaServer);
+
+            outVersoServer = new DataOutputStream(miosocket.getOutputStream());
+            inDalServer = new BufferedReader(new InputStreamReader(miosocket.getInputStream()));
+
+
+        } catch(UnknownHostException e){
+            System.err.println("Host sconosciuto"); 
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante la connessione!");
+            System.exit(1);
+        }
+        return miosocket;
+    }
+
+    public void comunica(){
+        try{
+            System.out.println("4 ... inserisci la strnga da trasmettere al server: " + '\n');
+            stringaUtente = tastiera.readLine();
+
+            System.out.println("5 ... invio la stringa al servr e attendo ...");
+            outVersoServer.writeBytes(stringaUtente + '\n');
+
+            stringaRicevutaDalServer = inDalServer.readLine();
+            System.out.println("8 ...  risposta dal server" + '\n' + stringaRicevutaDalServer);
+
+            System.out.println("9 CLIENT: termina elaborazione e chiude connessione");
+            miosocket.close();
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante la comunicazione col server");
+            System.exit(1);
+        }
+    }
+
+}
